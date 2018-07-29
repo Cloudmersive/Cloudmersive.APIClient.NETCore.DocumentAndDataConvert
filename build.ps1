22a926dd358b72c59816f7e1866fb5adc1e92b74
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2018-07-28 17:27:42 -0700

    Added

--- build.ps1
+++ build.ps1
@@ -3,3 +3,4 @@
 #(Get-Content ./client/src/api/ConvertDocumentApi.js).replace('var returnType = Object;', "var returnType = 'Blob';") | Set-Content ./client/src/api/ConvertDocumentApi.js
 #(Get-Content ./client/src/api/ConvertWebApi.js).replace('var returnType = Object;', "var returnType = 'Blob';") | Set-Content ./client/src/api/ConvertWebApi.js
 #& npm build ./client
+& dotnet build ./client/src/Cloudmersive.APIClient.NETCore.DocumentAndDataConvert/Cloudmersive.APIClient.NETCore.DocumentAndDataConvert.csproj -c Release
